419002a0139a6d2979f94e33b8b6c07e409a49a4
catluynh <catluynh99@gmail.com>
2021-11-08 12:04:27 +0700

    crud hash

---
+++ src/main/java/com/example/redis_crud_dcl_18087531/model/Employee.java
@@ -0,0 +1,17 @@
+package com.example.redis_crud_dcl_18087531.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Employee implements Serializable {
+    private int id;
+    private String name;
+
+}
